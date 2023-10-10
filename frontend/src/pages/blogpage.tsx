e5c9306c9f2999487528f1a50bd435aed426d184
sarikabhatta <sarikabhatta5@gmail.com>
2023-10-10 16:33:48 +0545

    feat: made sample blog page

---
+++ frontend/src/pages/blogpage.tsx
@@ -0,0 +1,98 @@
+import Header from "@/components/header";
+
+export default function UrbanFarmingPage() {
+  return (
+    <main>
+      <Header />
+
+      <div className="flex justify-center items-center mt-16">
+        <div className="w-3/4 rounded-lg">
+          <div className="flex items-center mb-6">
+            <div className="w-12 h-12 rounded-full flex items-center justify-center">
+              <img
+                src="images/farmerphotoblog.jpg"
+                alt="Farmer Icon"
+                className="w-8 h-8"
+              />
+            </div>
+            <div className="ml-4">
+              <p className="text-slate-500 font-semibold">Ram Adhikari</p>
+              <p className="text-slate-500">Written on June 30, 2023</p>
+            </div>
+          </div>
+          <h1 className="text-4xl text-primary-dark font-bold mb-4">
+            How to Start Urban Farming with Minimal Cost
+          </h1>
+          <img
+            src="images/urban-farming-1.jpg"
+            alt="Urban Farming"
+            className="mb-4 rounded-lg mx-0"
+          />
+          <p className="text-slate-500 text leading-6">
+            Urban farming offers numerous benefits, including access to fresh,
+            organic produce and a sustainable way to contribute to a greener,
+            healthier city. The good news is that you can start your own urban
+            farm with minimal cost and resources.
+          </p>
+          <h2 className="text-2xl text-primary-dark mt-6 font-semibold">
+            Benefits of Urban Farming
+          </h2>
+          <ul className="list-disc list-inside text-slate-500 mt-2 ml-4">
+            <li>Reduce grocery bills by growing your own food.</li>
+            <li>Access fresh and organic produce right at home.</li>
+            <li>
+              Contribute to a more eco-friendly and sustainable lifestyle.
+            </li>
+            <li>Engage with your community through urban farming projects.</li>
+          </ul>
+          <br />
+
+          <p className="text-slate-500">
+            We can start urban farming using minimal cost. Following are the
+            necessary steps:
+          </p>
+
+          <h1 className="text-xl font-bold">Step 1</h1>
+
+          <p className="text-slate-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+
+          <img src="images/urban-farming-1.jpg" className="h-60 pt-2"></img>
+
+          <h1 className="text-xl font-bold">Step 2</h1>
+
+          <p className="text-slate-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+
+          <img src="images/urban-farming-2.jpg" className="h-60 pt-2"></img>
+
+          <h1 className="text-xl font-bold">Step 3</h1>
+
+          <p className="text-slate-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+
+          <img src="images/urban-farming-3.png" className="h-60 pt-2"></img>
+
+          <h1 className="text-xl font-bold">Step 4</h1>
+
+          <p className="text-slate-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+
+          <img src="images/urban-farming-1.jpg" className="h-60 pt-2"></img>
+          <div className="mt-8">
+            <a
+              href="#"
+              className="text-primary hover:underline text-lg font-semibold"
+            >
+              Learn More About Urban Farming
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- frontend/src/pages/kheti-shikshya.tsx
+++ frontend/src/pages/kheti-shikshya.tsx
@@ -1,108 +1,184 @@
-export default function KhetiShikshya() {
-    return (
-        <>
-        <div>
-    <p className="text-center p-1 mt-50 text-green-900 text-4xl font-bold mb-2 border-b-8 border-green-600 text-center w-max mx-auto">Kheti Shikshya</p>
-    
-    <div><p className="text-center px-60 mt-5 text-slate-600 text-lg mb-2 ">Kheti Shiksya is a central hub for learning more about Urban farming. It includes official curated guides
-        along with a huge array of articles shared by urban farmers community.</p>
-    </div>
-</div>
- 
-    <div>
-        <div
-        className="bg-white px-10 p-4 rounded-lg  flex w-1/8">
-    <div className="bg-white p-4 rounded-lg shadow flex w-1/8">
-        <img src="aero.jpg" alt="Image 1" className="w-1/8 h=auto"/>
-        <div>
-            <p className="ml-4 font-medium "> How I got started with vertical farming and you can too.</p>
-        <p className="ml-4"> Let me take you on a journey of how I got started with vertical farming which helped me increase quantity of my produces</p>
-        </div>
-        
-    </div>
-/
-    <div className="bg-white p-4 rounded-lg shadow flex">
-        <img src="tools.jpg" alt="Image 2" className="w-1/8 h-auto"/>
-        <div>
-        <p className="ml-4 font-medium">Must have equipments for Urban Farmers for max productivity.</p>
-        <p className="ml-4">Let me take you on a journey of how I got started with vertical farming which helped me increase quantity of my produces</p>
-        </div>
-        
-    </div>
-    </div>
-    <div
-    className="bg-white  px-10 p-4 rounded-lg flex w-1/8">
-    
-<div className="bg-white p-4 rounded-lg shadow flex w-1/8">
-    <img src="carrot.jpg" alt="Image 1" className="w-1/8 h=auto"/>
-    <div>
-        <p className="ml-4 font-medium">10 reasons why carrots are the best crop for Urban farmers</p>
-    <p className="ml-4"> Let me take you on a journey of how I got started with vertical farming which helped me increase quantity of my produces</p>
-    </div>
-    
-</div>
+import Header from "@/components/header";
 
-<div className="bg-white p-4 rounded-lg shadow flex">
-    <img src="strawbery.jpg" alt="Image 2" className="w-1/8 h-auto"/>
+export default function KhetiShikshya() {
+  return (
     <div>
-    <p className="ml-4 font-medium">How to farm strawberries in a large scale.</p>
-    <p className="ml-4">Let me take you on a journey of how I got started with vertical farming which helped me increase quantity of my produces</p>
-    </div>
-    
-</div>
-</div>
-    </div>
-    <div>
-        <p className="text-left px-10 mt-50 text-green-900 text-2xl font-bold mb-2 ">Community Articles</p>
-        <div><p className="text-left px-10 mt-2 text-slate-600 text-lg mb-2 ">Articles written by urban farmers.</p>
-        </div>
-    </div>
-   
-     
+      <Header />
+      <div>
+        <p className="text-center p-1 mt-50 text-green-900 text-4xl font-bold mb-2 border-b-8 border-green-600 text-center w-max mx-auto">
+          Kheti Shikshya
+        </p>
+
         <div>
-            <div
-            className="bg-white px-10 p-4 rounded-lg flex w-1/8">
-            
-        <div className="bg-white p-4 rounded-lg shadow flex w-1/8">
-            <img src="aero.jpg" alt="Image 1" className="w-1/8 h=auto"/>
+          <p className="text-center px-60 mt-5 text-slate-600 text-lg mb-2 ">
+            Kheti Shiksya is a central hub for learning more about Urban
+            farming. It includes official curated guides along with a huge array
+            of articles shared by urban farmers community.
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <div className="bg-white px-10 p-4 rounded-lg  flex w-1/8">
+          <div className="bg-white p-2 rounded-lg shadow flex w-1/8">
+            <img
+              src="images/cabbage.png"
+              alt="Image 1"
+              className="w-1/8 h=auto"
+            />
             <div>
-                <p className="ml-4 font-medium"> How I got started with vertical farming and you can too.</p>
-            <p className="ml-4"> Let me take you on a journey of how I got started with vertical farming which helped me increase quantity of my produces</p>
+              <p className="ml-4 font-medium ">
+                {" "}
+                How I got started with vertical farming and you can too.
+              </p>
+              <p className="ml-4">
+                {" "}
+                Let me take you on a journey of how I got started with vertical
+                farming which helped me increase quantity of my produces
+              </p>
             </div>
-            
-        </div>
-    
-        <div className="bg-white p-4 rounded-lg shadow flex">
-            <img src="tools.jpg" alt="Image 2" className="w-1/8 h-auto"/>
+          </div>
+
+          <div className="bg-white p-4 rounded-lg shadow flex">
+            <img
+              src="images/tools.png"
+              alt="Image 2"
+              className="w-1/8 h-auto"
+            />
             <div>
-            <p className="ml-4 font-medium">Must have equipments for Urban Farmers for max productivity.</p>
-            <p className="ml-4">Let me take you on a journey of how I got started with vertical farming which helped me increase quantity of my produces</p>
+              <p className="ml-4 font-medium">
+                Must have equipments for Urban Farmers for max productivity.
+              </p>
+              <p className="ml-4">
+                Let me take you on a journey of how I got started with vertical
+                farming which helped me increase quantity of my produces
+              </p>
             </div>
-            
+          </div>
         </div>
+        <div className="bg-white  px-10 p-4 rounded-lg flex w-1/8">
+          <div className="bg-white p-4 rounded-lg shadow flex w-1/8">
+            <img
+              src="images/carrot.png"
+              alt="Image 1"
+              className="w-1/8 h=auto"
+            />
+            <div>
+              <p className="ml-4 font-medium">
+                10 reasons why carrots are the best crop for Urban farmers
+              </p>
+              <p className="ml-4">
+                {" "}
+                Let me take you on a journey of how I got started with vertical
+                farming which helped me increase quantity of my produces
+              </p>
+            </div>
+          </div>
+
+          <div className="bg-white p-4 rounded-lg shadow flex">
+            <img
+              src="images/strawberry.png"
+              alt="Image 2"
+              className="w-1/8 h-auto"
+            />
+            <div>
+              <p className="ml-4 font-medium">
+                How to farm strawberries in a large scale.
+              </p>
+              <p className="ml-4">
+                Let me take you on a journey of how I got started with vertical
+                farming which helped me increase quantity of my produces
+              </p>
+            </div>
+          </div>
         </div>
-        <div
-        className="bg-white px-10 p-4 rounded-lg flex w-1/8">
-         
-    <div className="bg-white p-4 rounded-lg shadow flex w-1/8">
-        <img src="carrot.jpg" alt="Image 3" className="w-1/8 h=auto"/>
+      </div>
+      <div>
+        <p className="text-left px-10 mt-50 text-green-900 text-2xl font-bold mb-2 ">
+          Community Articles
+        </p>
         <div>
-            <p className="ml-4 font-medium">10 reasons why carrots are the best crop for Urban farmers</p>
-        <p className="ml-4"> Let me take you on a journey of how I got started with vertical farming which helped me increase quantity of my produces</p>
+          <p className="text-left px-10 mt-2 text-slate-600 text-lg mb-2 ">
+            Articles written by urban farmers.
+          </p>
         </div>
-        
-    </div>
-    
-    <div className="bg-white p-4 rounded-lg shadow flex">
-        <img src="strawbery.jpg" alt="Image 4" className="w-1/8 h-auto"/>
-        <div>
-        <p className="ml-4 font-medium">How to farm strawberries in a large scale.</p>
-        <p className="ml-4">Let me take you on a journey of how I got started with vertical farming which helped me increase quantity of my produces</p>
+      </div>
+
+      <div>
+        <div className="bg-white px-10 p-4 rounded-lg flex w-1/8">
+          <div className="bg-white p-4 rounded-lg shadow flex w-1/8">
+            <img
+              src="images/cabbage.png"
+              alt="Image 1"
+              className="w-1/8 h=auto"
+            />
+            <div>
+              <p className="ml-4 font-medium">
+                {" "}
+                How I got started with vertical farming and you can too.
+              </p>
+              <p className="ml-4">
+                {" "}
+                Let me take you on a journey of how I got started with vertical
+                farming which helped me increase quantity of my produces
+              </p>
+            </div>
+          </div>
+
+          <div className="bg-white p-4 rounded-lg shadow flex">
+            <img
+              src="images/tools.png"
+              alt="Image 2"
+              className="w-1/8 h-auto"
+            />
+            <div>
+              <p className="ml-4 font-medium">
+                Must have equipments for Urban Farmers for max productivity.
+              </p>
+              <p className="ml-4">
+                Let me take you on a journey of how I got started with vertical
+                farming which helped me increase quantity of my produces
+              </p>
+            </div>
+          </div>
         </div>
-        
-    </div>
-    </div>
+        <div className="bg-white px-10 p-4 rounded-lg flex w-1/8">
+          <div className="bg-white p-4 rounded-lg shadow flex w-1/8">
+            <img
+              src="images/carrot.png"
+              alt="Image 3"
+              className="w-1/8 h=auto"
+            />
+            <div>
+              <p className="ml-4 font-medium">
+                10 reasons why carrots are the best crop for Urban farmers
+              </p>
+              <p className="ml-4">
+                {" "}
+                Let me take you on a journey of how I got started with vertical
+                farming which helped me increase quantity of my produces
+              </p>
+            </div>
+          </div>
+
+          <div className="bg-white p-4 rounded-lg shadow flex">
+            <img
+              src="images/strawberry.png"
+              alt="Image 4"
+              className="w-1/8 h-auto"
+            />
+            <div>
+              <p className="ml-4 font-medium">
+                How to farm strawberries in a large scale.
+              </p>
+              <p className="ml-4">
+                Let me take you on a journey of how I got started with vertical
+                farming which helped me increase quantity of my produces
+              </p>
+            </div>
+          </div>
         </div>
-</>
-    )
+      </div>
+    </div>
+  );
 }
